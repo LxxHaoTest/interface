6af8991c3ba75d2a0b363723dc2a13909c41cdbe
LxxHaoTest <39108356+LxxHaoTest@users.noreply.github.com>
2018-05-31 11:51:16 +0800

    added

---
+++ Discuz_project/src/test/java/org/test/gun/TestLogin.java
@@ -0,0 +1,15 @@
+package org.test.gun;
+
+import org.testng.annotations.*;
+
+public class TestLogin {
+@BeforeClass
+ public void setUp() {
+	System.out.println("HelloWorld");
+}
+@Test
+public void a() {
+	System.out.println("Hello");
+}
+
+}
---
+++ Discuz_project/src/test/java/org/test/gun/TestLogin1.java
@@ -0,0 +1,13 @@
+package org.test.gun;
+import org.testng.annotations.*;
+public class TestLogin1 {
+
+	@BeforeClass
+	public void setUp() {
+	}
+	@Test
+	public void b() {
+		System.out.println("Hi");
+		
+	}
+}
